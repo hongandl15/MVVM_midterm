1a0f87a16add8bba36fe67fd86a4079343241375
An Nguyen <cuvitdet15@gmail.com>
2022-05-09 10:08:13 +0700

    MVVM C#

---
+++ MVVM/MVVM/Model/HelloWorldModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM.Model
+{
+    public class HelloWorldModel
+    {
+        private List<string> repositoryData;
+        public string ImportantInfo
+        {
+            get
+            {
+                return ConcatenateData(repositoryData);
+            }
+        }
+
+        public HelloWorldModel()
+        {
+            repositoryData = GetData();
+        }
+
+       
+        private List<string> GetData()
+        {
+            repositoryData = new List<string>()
+            {
+                "Hello",
+                "world"
+            };
+            return repositoryData;
+        }
+
+     
+        private string ConcatenateData(List<string> dataList)
+        {
+            string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+            return importantInfo;
+        }
+    }
+}
---
+++ MVVM/MVVM/ViewModel/HelloWorldViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM.Model;
+
+namespace MVVM.ViewModel
+{
+   
+    public class HelloWorldViewModel : INotifyPropertyChanged
+    {
+        private string helloString;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return helloString;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public HelloWorldViewModel()
+        {
+            HelloWorldModel helloWorldModel = new HelloWorldModel();
+            helloString = helloWorldModel.ImportantInfo;
+        }
+    }
+}
