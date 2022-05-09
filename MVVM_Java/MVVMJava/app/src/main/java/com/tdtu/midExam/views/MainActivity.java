96240f5529837dabc8c8f372fa3af3d7a630b0fc
An Nguyen <cuvitdet15@gmail.com>
2022-05-09 10:14:51 +0700

    MVVM java

---
+++ MVVM_Java/MVVMJava/app/src/main/java/com/tdtu/midExam/views/MainActivity.java
@@ -0,0 +1,33 @@
+package com.tdtu.midExam.views;
+
+
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.tdtu.midExam.R;
+import com.tdtu.midExam.databinding.ActivityMainBinding;
+import com.tdtu.midExam.viewmodels.LoginViewModel;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new LoginViewModel());
+
+        activityMainBinding.executePendingBindings();
+
+    }
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+}
